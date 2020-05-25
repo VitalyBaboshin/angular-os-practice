fdc3a4aa92757221aa3f9b806d56775f13bf0172
VitalyBaboshin <vitaliy2@ro.ru>
2020-05-25 23:47:27 +0500

    Добавлена логика интерсептора, обработка ошибки 401

---
+++ src/app/shared/auth.interceptor.ts
@@ -0,0 +1,40 @@
+import {Injectable} from "@angular/core";
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
+import {AuthServices} from "../admin/shared/services/auth.services";
+import {Router} from "@angular/router";
+import {catchError} from "rxjs/operators";
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor{
+  constructor(
+    private auth: AuthServices,
+    private router: Router
+  ) {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (this.auth.isAuthenticated()) {
+      req = req.clone({
+        setParams: {
+          auth: this.auth.token
+        }
+      });
+    }
+
+    return next.handle(req)
+      .pipe(
+        catchError ( (error: HttpErrorResponse) => {
+          if (error.status === 401) {
+            this.auth.logout();
+            this.router.navigate(['/admin', 'login'], {
+              queryParams: {
+                authFailed: true
+              }
+            });
+          }
+          return throwError(error);
+        })
+      )
+  }
+}
