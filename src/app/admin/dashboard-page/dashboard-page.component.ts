e6bc015ab8ddb812d2ef76494b4b130cab98fe17
VitalyBaboshin <vitaliy2@ro.ru>
2020-05-26 23:40:34 +0500

    Добавлено при инициализации загрузка товаров, + метод удаления товара

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ProductService} from "../../shared/product.service";
+import {Product} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  products: Product[] = [];
+  pSubscription: Subscription;
+  dSubscription: Subscription;
+  productName: string;
+  constructor(
+    private productService: ProductService
+  ) { }
 
   ngOnInit(): void {
+    this.pSubscription = this.productService.getAll().subscribe(products => {
+      this.products = products;
+    })
+  }
+
+  remove(id: string) {
+    this.dSubscription = this.productService.remove(id).subscribe(() => {
+      this.products = this.products.filter( product => product.id !== id);
+    })
+  }
+
+  ngOnDestroy(): void {
+    if (this.pSubscription) {
+      this.pSubscription.unsubscribe();
+    }
+    if (this.dSubscription) {
+      this.dSubscription.unsubscribe();
+    }
   }
 
 }
