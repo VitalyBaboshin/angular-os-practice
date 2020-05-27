e7ab46917e2586722da0444ef354850500e1a3be
VitalyBaboshin <vitaliy2@ro.ru>
2020-05-27 20:18:48 +0500

    add

---
+++ src/app/shared/sorting.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Product} from "./interfaces";
+
+@Pipe({
+  name: 'sorting'
+})
+export class SortingPipe implements PipeTransform {
+
+  transform(products: Product[], type = ''): Product[] {
+
+    if (type === 'All') {
+      return products;
+    }
+    return products.filter( product => {
+      return product.type == type;
+    })
+  }
+
+}
