d4448e9a5f5624e13326b70f91f42922577c0347
VitalyBaboshin <vitaliy2@ro.ru>
2020-06-01 15:05:35 +0500

    Без изменений

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ const routes: Routes = [
       {path: 'product/:id', component: ProductPageComponent},
       {path: 'cart', component: CartPageComponent},
       {path: '', redirectTo: '/', pathMatch: 'full'}
+
     ]
   },
   {path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)}
